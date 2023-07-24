a31ff1b4f97e645d878e96b539b184d1ffbba0e9
Gregory Domingos <58437322+gregdom@users.noreply.github.com>
2023-07-24 13:10:02 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Mozilla Firefox Store (Add-ons):
+    https://addons.mozilla.org/pt-BR/firefox/addon/ytbooster/
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
